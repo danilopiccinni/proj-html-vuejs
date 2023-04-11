4a49301ec776f6d0db5dff6679f11a8b424b9659
danilopiccinni <danilopiccinni@gmail.com>
2023-04-11 13:27:54 +0200

    feat: Header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,54 @@
+<script>
+import AppNav from './AppNav.vue';
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        components : {
+            AppNav,
+        }
+    }
+</script>
+
+<template>
+    <header>
+        <AppNav></AppNav>
+
+        <div class="titolo">
+            <h6>moving without the hassle</h6>
+            <h1>we make it simple</h1>
+            <h6>professional service with quality and customer satisfaction</h6>
+        </div>
+    </header>
+
+</template>
+
+<style scoped lang="scss">
+header {
+    height: 600px;
+    padding-top: 15px;
+
+    background-image: url(/img/avada-movers-homeherobackground-final.jpg);
+    background-position: top;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+.titolo {
+    text-align: center;
+    text-transform: uppercase;
+    padding: 200px;
+}
+
+h1 {
+    font-size: 45px;
+    font-weight: bold;
+}
+
+h6 {
+    color: #333333;
+    font-size: 10px;
+    font-weight: bold;
+}
+</style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,63 @@
+<script>
+    export default {
+        data() {
+            return {
+                navLinks: [
+                    'home',
+                    'rates',
+                    'testimonials',
+                    'FAQ',
+                    'blog',
+                    'contact',
+                    'FREE QOUTE',
+
+                ]
+            }
+        }
+    }
+</script>
+
+<template>
+    <nav>
+        <div class="container cont-nav">
+            <div>
+                <img src="/img/avada-movers-logo.png" alt="">
+            </div>
+    
+            <div>
+                <ul>
+                    <li v-for="link in navLinks"> {{ link }}</li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped lang="scss">
+
+.cont-nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+    ul {
+        display: flex;
+        align-items: center;
+        gap: 50px;
+        list-style-type: none;
+    }
+
+    li {
+        font-size: 12px;
+        font-weight: bold;
+        text-transform: capitalize;
+    }
+
+    li:last-child {
+        color: white;
+        background-color: #1675BF;
+        border-radius: 15px;
+        padding: 5px 15px ;
+    }
+}
+</style>
