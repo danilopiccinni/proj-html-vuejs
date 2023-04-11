d2df333275ebd71b3263af4ac79c260c1c3af3fc
danilopiccinni <danilopiccinni@gmail.com>
2023-04-11 14:31:19 +0200

    feat: AppBannerBlue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script>
+    import AppBannerBlue from './components/AppBannerBlue.vue';
     import AppBannerGreen from './components/AppBannerGreen.vue';
     import AppHeader from './components/AppHeader.vue';
     export default {
@@ -8,6 +9,7 @@
             }
         },
         components : {
+            AppBannerBlue,
             AppBannerGreen,
             AppHeader,
 
@@ -20,6 +22,8 @@
     <AppBannerGreen></AppBannerGreen>
 
     <AppHeader></AppHeader>
+
+    <AppBannerBlue></AppBannerBlue>
 
 </template>
 
---
+++ src/components/AppBannerBlue.vue
@@ -0,0 +1,35 @@
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="banner-blue">
+        <i class="fa-regular fa-paper-plane"></i>
+        <span>get a free quote online now!</span>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+.banner-blue {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 15px;
+
+    height: 100px;
+    color: white;
+    background-color: #1675BF;
+
+    span {
+        text-transform: uppercase;
+    }
+}
+
+</style>
