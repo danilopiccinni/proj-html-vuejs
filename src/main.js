a93d13116f8c19ae382d7e5a86fb6a3871b86ca5
danilopiccinni <danilopiccinni@gmail.com>
2023-04-11 12:18:35 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.scss'
+import App from './App.vue'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+createApp(App).mount('#app')
