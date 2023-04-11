00f3575c4306346da7b2e9747a70d1c0e27eb7c2
danilopiccinni <danilopiccinni@gmail.com>
2023-04-11 15:39:54 +0200

    feat: section3

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script>
+    import AppSection3 from './components/AppSection3.vue';
     import AppSection2 from './components/AppSection2.vue'
     import AppSection1 from './components/AppSection1.vue';
     import AppBannerBlue from './components/AppBannerBlue.vue';
@@ -11,6 +12,7 @@
             }
         },
         components : {
+            AppSection3,
             AppSection2,
             AppSection1,
             AppBannerBlue,
@@ -32,6 +34,8 @@
     <AppSection1></AppSection1>
 
     <AppSection2></AppSection2>
+
+    <AppSection3></AppSection3>
 
 </template>
 
---
+++ src/components/AppSection3.vue
@@ -0,0 +1,88 @@
+<script>
+    export default {
+        data() {
+            return {
+                card: 
+                    {
+                        smallTitle : 'we remove the headache from moving',
+                        title : 'move whit ease',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="section3">
+        <div class="container cont-section3">
+            <div class="_card">
+                <h6> {{ card.smallTitle }}</h6>
+                <h3> {{ card.title }}</h3>
+                <hr>
+                <p> {{ card.description }}</p>
+                <a href="">get a free quote now</a>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+
+.section3 {
+    height: 750px;
+    background-image: url(/img/avada-movers-movewithease-background-new.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: top;
+}
+
+.container {
+    display: flex;
+    justify-content: end;
+    padding-top: 250px;
+
+}
+
+._card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    width: 450px;
+    background-color: #6abf16;
+
+    border: 10px solid #539511;
+
+    padding: 25px;
+
+    color: white;
+}
+
+a {
+    text-decoration: none;
+    color: white;
+    align-self: center;
+    background-color: #1675bf;
+    padding: 5px 15px;
+    border-radius: 15px;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 12px;
+}
+
+h3 {
+    font-size: 35px;
+    text-transform: capitalize;
+    font-weight: bold;
+}
+
+h6 {
+    text-transform: uppercase;
+    font-size: 10px;
+}
+
+
+
+</style>
