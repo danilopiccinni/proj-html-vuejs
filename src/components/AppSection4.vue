61e2fd32d995374409bdff942d62102c743fcfca
danilopiccinni <danilopiccinni@gmail.com>
2023-04-12 15:58:05 +0200

    feat:slider section4 testimonial

--- src/components/AppSection4.vue
+++ src/components/AppSection4.vue
@@ -1,4 +1,5 @@
 <script>
+    import AppSlider from './AppSlider.vue';
     export default {
         data() {
             return {
@@ -17,36 +18,170 @@
                         image : '/img/avada-movers-johnsmith-final.jpg',
                         name : 'john smith',
                         description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
-                    }
+                    },
+                    {
+                        image : '/img/avada-movers-johndoe-final.jpg',
+                        name : 'john doe',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-janedoe-final.jpg',
+                        name : 'jane doe',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-johnsmith-final.jpg',
+                        name : 'john smith',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-johndoe-final.jpg',
+                        name : 'john doe',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-janedoe-final.jpg',
+                        name : 'jane doe',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-johnsmith-final.jpg',
+                        name : 'john smith',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-johndoe-final.jpg',
+                        name : 'john doe',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-janedoe-final.jpg',
+                        name : 'jane doe',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-johnsmith-final.jpg',
+                        name : 'john smith',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-johndoe-final.jpg',
+                        name : 'john doe',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-janedoe-final.jpg',
+                        name : 'jane doe',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-johnsmith-final.jpg',
+                        name : 'john smith',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-johndoe-final.jpg',
+                        name : 'john doe',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-janedoe-final.jpg',
+                        name : 'jane doe',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-johnsmith-final.jpg',
+                        name : 'john smith',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-johndoe-final.jpg',
+                        name : 'john doe',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-janedoe-final.jpg',
+                        name : 'jane doe',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-johnsmith-final.jpg',
+                        name : 'john smith',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-johndoe-final.jpg',
+                        name : 'john doe',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-janedoe-final.jpg',
+                        name : 'jane doe',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
+                    {
+                        image : '/img/avada-movers-johnsmith-final.jpg',
+                        name : 'john smith',
+                        description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, aliquam ratione aperiam quasi neque nulla tempora ullam, veniam quaerat ex dolor nemo odit sapiente qui quibusdam deleniti obcaecati autem earum!',
+                    },
                 ]
             }
+        },
+
+        components : {
+            AppSlider,
         }
     }
 </script>
 
 <template>
-    <div class="section4">
+    <!-- <div class="section4">
         <div class="container padding cont-section4">
             <div>
                 <h6>happy customers</h6>
                 <h3 class="section-title">testimonials</h3>
             </div>
-            <div class="cont-cards flex-column flex-md-row row-cols-sm-1 row-cols-md-4">
-                <div v-for="person in persons" class="_card">
-                    <img :src="person.image" alt="">
-                    <h5> {{ person.name }}</h5>
-                    <p> <em>"{{ person.description }}"</em> </p>
+                <div class="cont-cards flex-column flex-md-row row-cols-sm-1 row-cols-md-4">
+                    <div v-for="person in persons" class="_card">
+                        <img :src="person.image" alt="">
+                        <h5> {{ person.name }}</h5>
+                        <p> <em>"{{ person.description }}"</em> </p>
+                    </div>
                 </div>
-            </div>
             <div>
                 <a class="link" href="">read more testmonials</a>
             </div>
         </div>
-    </div>
+    </div> -->
+
+
+    <div class="section4">
+        <div class="padding cont-section4">
+            <div>
+                <h6>happy customers</h6>
+                <h3 class="section-title">testimonials</h3>
+            </div>
+
+                <AppSlider>
+                    <div v-for="(person, index) in persons" :key="index" class="_card">
+                        <img :src="person.image" alt="immagine">
+                        <h5> {{ person.name }}</h5>
+                        <p> <em>"{{ person.description }}"</em> </p>
+                    </div>
+                </AppSlider>
+
+            <div>
+                <a class="link" href="">read more testmonials</a>
+            </div>
+        </div>
+    </div> 
+
 
 </template>
 
 <style scoped lang="scss">
+
+
 
 .section4 {
     background-image: url(/img/avada-movers-testimonial-background.jpg);
@@ -54,27 +189,20 @@
     background-position: 50% 25%;
 }
 
+.cont-section4 {
+    display: flex;
+    flex-flow: column nowrap;
+    text-align: center;
+    gap: 50px;
+}
+
 img {
-    width: 100px;
+    width: calc(100vw /5);
     border-radius: 50%;
 }
 
-.cont-section4 {
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    gap: 50px;
-    text-align: center;
-
-
-}
-.cont-cards {
-    display: flex;
-    justify-content: space-between;
-    gap: 50px;
-}
-
 ._card {
+
     display: flex;
     flex-direction: column;
     justify-content: center;
---
+++ src/components/AppSlider.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  data() {
+    return {
+      offset: -1550,
+      step: 150 // la larghezza di ogni immagine
+    };
+  },
+  methods: {
+    scroll(direction) {
+      this.offset += this.step * direction;
+    }
+  },
+
+
+};
+</script>
+
+<template>
+    <div class="slider-laterale">
+      <div class="slider-content" :style="{ transform: 'translateX(' + offset + 'px)' }">
+        <slot></slot>
+      </div>
+      <div class="slider-controls">
+        <button @click="scroll(3)"><i class="fa-solid fa-circle-chevron-left"></i></button>
+        <button @click="scroll(-3)"><i class="fa-solid fa-circle-chevron-right"></i></button>
+      </div>
+    </div>
+  </template>
+  
+  <style scoped lang="scss">
+  .slider-laterale {
+    padding-bottom: 50px;
+    position: relative;
+    overflow: hidden;
+    
+  }
+  .slider-content {
+    display: flex;
+    justify-content: center;
+    gap: 25px;
+    transition: transform 0.5s;
+  }
+  .slider-controls {
+
+    position: absolute;
+    bottom: -5px;
+    right: 50%;
+    transform: translateX(50%);
+  
+  }
+  .slider-controls button {
+    background-color: transparent;
+    color: #1675bf;
+    border: none;
+
+    font-size: 2rem;
+    padding: 0 1rem;
+    cursor: pointer;
+
+    &:hover {
+      color: #2e9cf0;
+    }
+  }
+  </style>
